4dcbc8c68b4e2d17109a3ad4dbf9357a53a45857
BCavala <cabruno00@gmail.com>
2023-11-08 23:46:18 +0100

    Revert "Task 2 - Adding Data-Persistence Features to the Contact Repository Implementation"
    This reverts commit 56b74e071ccfc8f38e2e1382270fc8f1a937ffbf.

--- ContactManager/ContactManager/Services/ConctactRepository.cs
+++ ContactManager/ContactManager/Services/ConctactRepository.cs
@@ -1,79 +1,21 @@
 ﻿namespace ContactManager.Services;
-using Microsoft.AspNetCore.Http;
 using ContactManager.Models;
-
 public class ContactRepository
 {
-    private const string CacheKey = "ContactStore";
-    private readonly IHttpContextAccessor _httpContextAccessor;
-
-    public ContactRepository(IHttpContextAccessor httpContextAccessor)
-    {
-        _httpContextAccessor = httpContextAccessor;
-    }
-
     public Contact[] GetAllContacts()
     {
-        var ctx = _httpContextAccessor.HttpContext;
-
-        if (ctx != null)
-        {
-            return (Contact[])ctx.Items[CacheKey];
-        }
-
         return new Contact[]
         {
             new Contact
             {
-                Id = 0,
-                Name = "Placeholder"
+                Id = 1,
+                Name = "Glenn Block"
+            },
+            new Contact
+            {
+                Id = 2,
+                Name = "Dan Roth"
             }
         };
-    }
-
-    public ContactRepository()
-    {
-        var ctx = _httpContextAccessor.HttpContext;
-
-        if (ctx != null && ctx.Items[CacheKey] == null)
-        {
-            var contacts = new Contact[]
-            {
-                new Contact
-                {
-                    Id = 1, Name = "Glenn Block"
-                },
-                new Contact
-                {
-                    Id = 2, Name = "Dan Roth"
-                }
-            };
-
-            ctx.Items[CacheKey] = contacts;
-        }
-    }
-
-    public bool SaveContact(Contact contact)
-    {
-        var ctx = _httpContextAccessor.HttpContext;
-
-        if (ctx != null)
-        {
-            try
-            {
-                var currentData = ((Contact[])ctx.Items[CacheKey]).ToList();
-                currentData.Add(contact);
-                ctx.Items[CacheKey] = currentData.ToArray();
-
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return false;
-            }
-        }
-
-        return false;
     }
 }
